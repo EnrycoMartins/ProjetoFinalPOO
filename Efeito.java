a1218ab4f624851777587f4b281a6503f086713d
Cesar <cesarauneves@gmail.com>
2025-11-05 15:47:49 -0300

    caca

--- Efeito.java
+++ Efeito.java
@@ -1,7 +1,19 @@
 public enum Efeito {
-    CURA,
-    BUFF_ATAQUE,
-    BUFF_DEFESA,
-    DANO,
-    OUTRO
+    CURA(25),           // Cura 25 Pontos de Vida
+    BUFF_ATAQUE(5),   // Aumenta Ataque em 5
+    BUFF_DEFESA(3),   // Aumenta Defesa em 3
+    CURA_SINAL(2),    // Restaura 2 Sinais (para o Bruxo)
+    DANO(-10),          // Causa 10 de dano (ex: veneno)
+    OUTRO(0);           // Efeito nulo ou de-logica-custom
+
+    // --- Atributo ---
+    private int valor;
+
+    // --- Construtor ---
+    Efeito(int valor) {
+        this.valor = valor;
+    }
+    public int getValor() {
+        return this.valor;
+    }
 }
